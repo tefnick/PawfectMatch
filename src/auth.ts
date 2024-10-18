220f634a3f227f92641c9e9101656325bc293369
tefnick <nickruiz210@gmail.com>
2024-10-18 16:28:19 -0500

    add prisma migration file

--- src/auth.ts
+++ src/auth.ts
@@ -3,7 +3,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/lib/prisma"
 import authConfig from "./auth.config";
 
-export const { handlers: {GET, POST}, auth, signIn, signOut } = NextAuth({
+export const { handlers, signIn, signOut, auth } = NextAuth({
   callbacks: {
     async session({ token, session }) {
       if (token.sub && session.user) {
