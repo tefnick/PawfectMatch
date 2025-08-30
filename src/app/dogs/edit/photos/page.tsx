ba15f9138e05fad3b22f68fd74311af7bba1126f
tefnick <nickruiz210@gmail.com>
2025-08-30 15:26:01 -0500

    fix photo upload page

--- src/app/dogs/edit/photos/page.tsx
+++ src/app/dogs/edit/photos/page.tsx
@@ -1,28 +1,32 @@
-import { CardHeader, CardBody, Image } from '@nextui-org/react'
-import React from 'react'
-import Editform from '../Editform'
-import { getAuthUserId } from '@/app/actions/authActions'
-import { getDogByUserId, getDogPhotosByUserId } from '@/app/actions/dogActions';
-import StarButton from '@/components/StarButton';
-import DeleteButton from '@/components/DeleteButton';
-import ImageUploadButton from '@/components/ImageUploadButton';
-import DogPhotoUpload from './DogPhotoUpload';
-import DogImage from '@/components/DogImage';
-import DogPhotos from '@/components/DogPhotos';
-import CardInnerWrapper from '@/components/CardInnerWrapper';
+import { CardHeader, CardBody, Image, Divider } from "@nextui-org/react";
+import React from "react";
+import Editform from "../Editform";
+import { getAuthUserId } from "@/app/actions/authActions";
+import { getDogByUserId, getDogPhotosByUserId } from "@/app/actions/dogActions";
+import StarButton from "@/components/StarButton";
+import DeleteButton from "@/components/DeleteButton";
+import ImageUploadButton from "@/components/ImageUploadButton";
+import DogPhotoUpload from "./DogPhotoUpload";
+import DogImage from "@/components/DogImage";
+import DogPhotos from "@/components/DogPhotos";
+import CardInnerWrapper from "@/components/CardInnerWrapper";
 
 export default async function EditPhotosPage() {
-  const userId = await getAuthUserId() as string;
-  const dog = await getDogByUserId(userId)
+  const userId = (await getAuthUserId()) as string;
+  const dog = await getDogByUserId(userId);
   const photos = await getDogPhotosByUserId(userId);
   return (
     <>
-      <CardInnerWrapper 
-        header="Edit Photos"
-        body={
-          <DogPhotos photos={photos} mainImageUrl={dog?.image} editing/>
-        }
-      />
+      <CardHeader className="flex flex-row justify-between items-center">
+        <div className="text-2xl font-semibold test-secondary">
+          Edit Profile
+        </div>
+        <DogPhotoUpload />
+      </CardHeader>
+      <Divider />
+      <CardBody>
+        <DogPhotos photos={photos} editing={true} mainImageUrl={dog?.image} />
+      </CardBody>
     </>
-  )
+  );
 }
